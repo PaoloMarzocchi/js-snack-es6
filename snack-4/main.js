04917848bc53b99df007c2cb17429a216b12fa90
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-05 16:36:52 +0100

    add snack-4

--- snack-4/main.js
+++ snack-4/main.js
@@ -1,3 +1,47 @@
 /* Crea un array di oggetti che rappresentano delle persone.
 Ogni persona ha un nome, un cognome e un’età.
 Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età. */
+
+const peopleList = [
+    {
+        nome: 'Gianluca',
+        cognome: 'Sciapo',
+        eta: 17,
+    },
+    {
+        nome: 'Filippo',
+        cognome: 'Castro',
+        eta: 15,
+    },
+    {
+        nome: 'Eren',
+        cognome: 'Jaeger',
+        eta: 20,
+    },
+    {
+        nome: 'Gojo',
+        cognome: 'Satoru',
+        eta: 50,
+    },
+    {
+        nome: 'Carlo',
+        cognome: 'Zeno',
+        eta: 30,
+    },
+    {
+        nome: 'Levi',
+        cognome: 'Ackerman',
+        eta: 45,
+    },
+]
+
+const newPeopleList = peopleList.map(person => {
+    if (person.eta > 18) {
+        person = `Ciao ${person.nome} ${person.cognome} hai l'età per guidare!`
+    }else{
+        person = `Ciao ${person.nome} ${person.cognome} purtroppo non hai ancora l'età per guidare!`
+    }
+    return person;
+})
+
+console.log(newPeopleList);
