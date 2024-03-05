2429290744ca2cd02c161c7ffa4bf300bdab336e
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-05 16:22:53 +0100

    add snack-3

--- snack-3/main.js
+++ snack-3/main.js
@@ -7,3 +7,45 @@ Es:
   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
 ]
 Crea un nuovo array con la lista dei mammiferi. */
+
+const animalsList = [
+    {
+      nome: "Leone",
+      famiglia: "Felidi",
+      classe: "Mammiferi"
+    },
+    {
+      nome: "Orso",
+      famiglia: "Ursidi",
+      classe: "Mammiferi"
+    },
+    {
+      nome: "Aquila",
+      famiglia: "Accipitridi",
+      classe: "Uccelli"
+    },
+    {
+      nome: "Tonno",
+      famiglia: "Scombridi",
+      classe: "Pesci"
+    },
+    {
+      nome: "Elefante",
+      famiglia: "Elefantiidi",
+      classe: "Mammiferi"
+    },
+    {
+      nome: "Cane",
+      famiglia: "Canidi",
+      classe: "Mammiferi"
+    },
+    {
+      nome: "Coccodrillo",
+      famiglia: "Coccodrillidi",
+      classe: "Rettili"
+    },
+  ];
+
+  const mammalsList = animalsList.filter(animal => animal.classe === 'Mammiferi')
+
+  console.log(mammalsList);
