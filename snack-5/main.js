bf1515d5b8533ac56dfd0be1f5e71319b6a9f6dc
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-06 16:19:55 +0100

    initial commit

---
+++ snack-5/main.js
@@ -0,0 +1,8 @@
+/* *SNACK 5*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+*BONUS*
+Stampare in pagina oltre che in console!
+*/
