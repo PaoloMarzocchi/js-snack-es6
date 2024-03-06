c5a98f3f95dad51d820f6c7a62db055a0f7656f8
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-06 17:36:41 +0100

    add snack-6 not working

--- snack-6/main.js
+++ snack-6/main.js
@@ -8,3 +8,55 @@ Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengo
 
 *BONUS*
 Stampare in pagina oltre che in console! */
+
+const footballTeams = [
+    {
+        nome: "Milan",
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Bologna",
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Atalanta",
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Genoa",
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Roma",
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Fiorentina",
+        punti: 0,
+        falliSubiti: 0
+    }
+];
+
+footballTeams.forEach(team => {
+
+    team.punti = randomNumb(0, 100);
+    team.falliSubiti = randomNumb(0, 30);
+});
+console.log(footballTeams);
+
+let noPointFootballTeams = footballTeams.filter(team => {
+    delete team.punti;
+    return team;
+});
+
+console.log(noPointFootballTeams);
+
+
+function randomNumb(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
